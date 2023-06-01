c5f9369e2ef242dd62269cc2cc104182b3f8aec4
Alexis Faure <alexis.faure@epitech.eu>
2023-06-01 14:51:51 +0200

    [FIX] - Fix lint

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import Router from "./router/router";
 import { RouterProvider } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react'
-import './style/tailwind.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import "./style/tailwind.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <RouterProvider router={Router} />
     </ChakraProvider>
   </React.StrictMode>,
-)
+);
