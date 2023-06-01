10cff98ef262762a181feaac4f30a235107b5e15
Alexis Faure <alexis.faure@epitech.eu>
2023-06-01 14:52:29 +0200

    [ADD] - Base axios request and state management to fetch cards using API
    [TODO] - Use react query

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import NavBar from "../components/Interface/NavBar";
+import CardContainer from "../components/Interface/CardContainer";
+import axios from "axios";
 
 function Home() {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        const fetchCards = async () => {
+            try {
+                const response = await axios.get("https://api.magicthegathering.io/v1/cards");
+                setCards(response.data.cards);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchCards();
+    }, []);
+
     return (
         <div>
+            <NavBar/>
             <h1>Home</h1>
             <p>Home page content</p>
+            <CardContainer cards={cards}/>
         </div>
     );
 }
